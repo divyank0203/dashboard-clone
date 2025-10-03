aaa40230a0a807678e92ae42f18c1301858de109
divyank0203 <divyankh444@gmail.com>
2025-10-03 18:55:44 +0530

    navbar update

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div className='flex justify-between mt-4 mb-4  shadow-lg'>
         <div className='flex justify-start'>
             <div className='ml-3 text-lg font-semibold'>Payouts</div>
-            <div className='flex justify-start ml-2'>
+            <div className='flex justify-start ml-2 hover:cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 ml-2 mt-2 text-gray-600">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
 </svg>
--- dashboard/src/components/Revenuebox.jsx
+++ dashboard/src/components/Revenuebox.jsx
@@ -23,7 +23,7 @@ function Revenuebox() {
     <div>
         <div className='shadow-md rounded-lg m-2 col-span-1'>
         <div className=' flex justify-start text-sm pl-3 pt-3 '>
-            <div className=''>Next Payout</div>
+            <div className=''>Amount Pending</div>
             <div className='pl-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
 </svg>
@@ -39,7 +39,7 @@ function Revenuebox() {
     <div>
                <div className=' rounded-lg m-2 col-span-1 shadow-md'>
         <div className=' flex justify-start text-sm pl-3 pt-3 '>
-            <div className=''>Next Payout</div>
+            <div className=''>Amount Processed</div>
             <div className='pl-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
 </svg>
